0bdea9d6a90140157f6c71e20beb89914f23e0c8
Jeff-Peters513 <petersjeff288@gmail.com>
2022-04-21 13:59:08 -0400

    init commit of TicTacToeGUI game

---
+++ TicTacToeGUI/src/ui/TicTacToeApp.java
@@ -0,0 +1,16 @@
+package ui;
+
+import business.TicTacToeObject;
+
+public class TicTacToeApp {
+
+	public static void main(String[] args) {
+		
+		TicTacToeObject tictactoe = new TicTacToeObject();
+		
+		System.out.println(tictactoe);
+		
+
+	}
+
+}
