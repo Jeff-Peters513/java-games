ff7e17692067c7a6cdd4947967286294e0f7e496
Jeff-Peters513 <petersjeff288@gmail.com>
2022-06-09 15:54:56 -0400

    init commit of snakegame and small change to tictactoe

---
+++ SnakeGame/src/ui/SnakeGame.java
@@ -0,0 +1,12 @@
+package ui;
+
+public class SnakeGame {
+	//bro code practice and game
+	
+	public static void main(String[] args) {
+
+		new GameFrame();		
+
+	}
+
+}
--- TicTacToe/src/ui/TicTacToeApp.java
+++ TicTacToe/src/ui/TicTacToeApp.java
@@ -2,7 +2,7 @@ package ui;
 
 import java.util.Random;
 import java.util.Scanner;
-
+ 
 public class TicTacToeApp {
 
 	public static void main(String[] args) {
