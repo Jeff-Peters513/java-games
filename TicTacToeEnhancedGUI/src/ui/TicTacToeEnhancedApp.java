876130a2415bb2497ecb656ce10e15bd0a408cb7
Jeff-Peters513 <petersjeff288@gmail.com>
2022-06-13 14:30:27 -0400

    init commit tictactoe enhanced version

---
+++ TicTacToeEnhancedGUI/src/ui/TicTacToeEnhancedApp.java
@@ -0,0 +1,14 @@
+package ui;
+
+import business.TicTacToeObj;
+
+public class TicTacToeEnhancedApp {
+
+	public static void main(String[] args) {
+		
+		//TicTacToeObj game = new TicTacToeObj();
+		new TicTacToeObj();
+		
+	}
+
+}
